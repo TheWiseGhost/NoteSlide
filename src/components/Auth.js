b468c17a81ce711e39aa4f1f2d9f76217462789e
Aditya Byju <byjuaditya@gmail.com>
2024-08-24 09:31:37 -0500

    try to fix cannonical issues

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -83,7 +83,7 @@ const Auth = () => {
           name="description"
           content="Log in to NoteSlide to access your personalized library of free, user-uploaded notes. Secure your study resources/information and make money from your notes."
         />
-        <link rel="canonical" href="https://note-slide.com/auth" />
+        <link rel="canonical" href="https://note-slide.com/auth/" />
       </Helmet>
       <img
         src="/images/NoteSlideLogo.png"
